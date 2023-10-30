7c875c283bd3b7856138fb404331a5a3d055ead1
Mike P <buhaytza2005@gmail.com>
2023-10-30 22:59:08 +0000

    feat: Binary search and the 2 crystal balls problem

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,22 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    // if the dataset is ordered, we can use the binary search
+    //
+    
+    let lo = 0;
+    let hi = haystack.length;
 
+    do {
+        let m = Math.floor(lo + (hi - lo) / 2);
+        let value = haystack[m];
+
+        if (value === needle) {
+            return true;
+        } else if ( value > needle) {
+            hi = m;
+        } else {
+            lo = m + 1;
+        }
+    } while (lo < hi);
+
+    return false
 }
